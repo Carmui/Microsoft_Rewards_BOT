45a56e1f56e370953723f62d8fbd1c783c87faa6
Carmui <mateusz3563@gmail.com>
2023-01-28 22:21:19 +0100

    Added driver function, ending phase, json file read/dump

--- Microsoft_Rewards_BOT.py
+++ Microsoft_Rewards_BOT.py
@@ -1,7 +1,7 @@
 import time
-import requests
+import os
 import json
-from termcolor import colored
+from colorama import Fore, Back, Style
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -18,9 +18,12 @@ class LoginException(Exception):
     pass
 
 
-# ---------------------------------- VARIABLES/OPTIONS CONIFG  ---------------------------------- #
+# ---------------------------------- VARIABLES/CHROME OPTIONS CONIFG  ---------------------------------- #
 
 BASE_URL = ""
+BASE_EMAIL = "EXAMPLE_EMAIL"
+BASE_PASSWORD = "EXAMPLE_PASSWORD"
+ACCOUNTS = []
 PC_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36 Edg/86.0.622.63'
 
 options = webdriver.ChromeOptions()
@@ -28,14 +31,24 @@ options.add_argument(f'user-agent={PC_USER_AGENT}')
 options.add_argument('disable-blink-features=AutomationControlled')
 options.add_experimental_option("detach", True)
 
-s = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s, options=options)
-driver.maximize_window()
+
+def driver_manager(opt: webdriver.ChromeOptions() = options) -> webdriver.Chrome:
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s, options=opt)
+    driver.maximize_window()
+    return driver
+
 
 # ---------------------------------- ADDITIONAL FUNCTIONS  ---------------------------------- #
 
 def login(browser: webdriver.Chrome, email: str, password: str):
     """ Logging into Microsoft Rewards Service """
+
+    print(Fore.CYAN + f""" 
+Login procedure for incoming account details:
+email: {email}
+password: {password}
+          """)
 
     # Getting access to the login website
     browser.get('https://login.live.com/')
@@ -74,22 +87,25 @@ def login(browser: webdriver.Chrome, email: str, password: str):
         # Wait 3 seconds
         time.sleep(3)
     except NoSuchElementException:
-        raise NoSuchElementException("This button doesn't exist. Please open the issue ticket URL: https://github.com/Carmui/Microsoft_Rewards_BOT/issues")
+        raise NoSuchElementException("This button doesn't exist. Please open the issue ticket.    |    URL: https://github.com/Carmui/Microsoft_Rewards_BOT/issues")
 
     except ElementNotInteractableException:
-        raise ElementNotInteractableException("Interaction problem. Please open the issue ticket URL: https://github.com/Carmui/Microsoft_Rewards_BOT/issues")
+        raise ElementNotInteractableException("Interaction problem. Please open the issue ticket.    |    URL: https://github.com/Carmui/Microsoft_Rewards_BOT/issues")
 
     except TimeoutException:
         raise ElementNotInteractableException("Timeout exception. Check all the login details.")
 
-    print(f'{email} status: ', colored('Logged-in !', 'green'))
-    # Check Login
-    if check_logging() is True:
-        result = colored('True', 'green')
-    else:
-        result = colored('False', 'red')
+    print(Fore.WHITE + f'{email} status: ', Fore.GREEN + 'Logged-in!')
 
-    print('Check logging function status (True - logged in, False - not logged in): ', result)
+    # Additional login check
+    if check_logging() is True:
+        color = Fore.GREEN
+        result = 'Logged-in!'
+    else:
+        color = Fore.RED
+        result = 'Not logged-in!'
+
+    print(Fore.WHITE + 'Additional check status:', color + result)
 
 
 def check_logging() -> bool:
@@ -98,7 +114,7 @@ def check_logging() -> bool:
 
 
 def is_element_Presence(browser: webdriver.Chrome, by: By, selector: str, delay: int):
-    """ Function dedicated to wait until element become visible """
+    """ Function dedicated to wait until element become presence """
     wait = WebDriverWait(browser, delay)
     wait.until(EC.presence_of_element_located((by, selector)))
 
@@ -109,6 +125,36 @@ def is_element_Clickable(browser: webdriver.Chrome, by: By, selector: str, delay
     wait.until(EC.element_to_be_clickable((by, selector)))
 
 
+def get_accounts(file_path: str) -> dict:
+    """
+        Get accounts from json file or create one if file doesn't exist.
+        Little advice: do not create more than 4 accounts (IP may be tracked and you will probably get banned)
+    """
+    try:
+        accounts = json.load(open(file_path, "r"))
+    except FileNotFoundError:
+        with open(file_path, 'w') as f:
+
+            f.write(json.dumps([
+            {
+                "email": BASE_EMAIL,
+                "password": BASE_PASSWORD
+            },
+            {
+                "email": BASE_EMAIL,
+                "password": BASE_PASSWORD
+            }
+            ], indent=5))
+
+
+        print("""
+[BOT] New file was created. Please update account details to proceed.
+[BOT] If you want to add more accounts -> follow the structure created in the file. New accounts need to be separeted with a comma.
+             """)
+        accounts = json.load(open(file_path, "r"))
+
+    return accounts
+
 def BOT_clickable_elements():
     """ Function dedicated to automate clicking process to earn points from Microsoft Rewards """
     pass
@@ -118,7 +164,37 @@ def BOT_writing_elements():
     """ Function dedicated to automate writing process to earn points from Microsoft Rewards """
     pass
 
+
+def close_phase(browser: webdriver.Chrome):
+    """ Function dedicated to close the BOT process. """
+    print(Fore.MAGENTA + """
+Work done. Quitting automation procedure.   
+--------------------------------------------------------------------------------------------------------------------------------
+    """)
+    browser.quit()
+
+
 # ---------------------------------- MAIN BOT PHASE  ---------------------------------- #
 
 if __name__ == "__main__":
-    login(driver, 'test', 'test')
+    print(Fore.CYAN + """
+                                ╔═╗╔═╗                      ╔═╗ ╔╗     ╔═══╗╔═══╗╔╗╔╗╔╗╔═══╗╔═══╗╔═══╗╔═══╗    ╔══╗ ╔═══╗╔════╗
+                                ║║╚╝║║                      ║╔╝╔╝╚╗    ║╔═╗║║╔══╝║║║║║║║╔═╗║║╔═╗║╚╗╔╗║║╔═╗║    ║╔╗║ ║╔═╗║║╔╗╔╗║
+                                ║╔╗╔╗║╔╗╔══╗╔═╗╔══╗╔══╗╔══╗╔╝╚╗╚╗╔╝    ║╚═╝║║╚══╗║║║║║║║║ ║║║╚═╝║ ║║║║║╚══╗    ║╚╝╚╗║║ ║║╚╝║║╚╝
+                                ║║║║║║╠╣║╔═╝║╔╝║╔╗║║══╣║╔╗║╚╗╔╝ ║║     ║╔╗╔╝║╔══╝║╚╝╚╝║║╚═╝║║╔╗╔╝ ║║║║╚══╗║    ║╔═╗║║║ ║║  ║║  
+                                ║║║║║║║║║╚═╗║║ ║╚╝║╠══║║╚╝║ ║║  ║╚╗    ║║║╚╗║╚══╗╚╗╔╗╔╝║╔═╗║║║║╚╗╔╝╚╝║║╚═╝║    ║╚═╝║║╚═╝║ ╔╝╚╗ 
+                                ╚╝╚╝╚╝╚╝╚══╝╚╝ ╚══╝╚══╝╚══╝ ╚╝  ╚═╝    ╚╝╚═╝╚═══╝ ╚╝╚╝ ╚╝ ╚╝╚╝╚═╝╚═══╝╚═══╝    ╚═══╝╚═══╝ ╚══╝ 
+                                                                 by Mateusz Michalowski (@carmui)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+    """)
+
+    # Getting accounts json path
+    accounts_path = os.path.dirname(os.path.abspath(__file__)) + '/accounts.json'
+    ACCOUNTS = get_accounts(accounts_path)
+
+    for account in ACCOUNTS:
+        # Getting browse driver
+        chrome_browser = driver_manager()
+
+        # Login to the server with the first account
+        login(chrome_browser, account["email"], account["password"])
+        close_phase(chrome_browser)
